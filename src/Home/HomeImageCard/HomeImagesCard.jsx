e83b1d66ced0bf3a42036be6c2e32d4672e8b59a
mamaducks <leonad308@gmail.com>
2022-04-11 23:39:28 -0400

    centered wrap home

--- src/Home/HomeImageCard/HomeImagesCard.jsx
+++ src/Home/HomeImageCard/HomeImagesCard.jsx
@@ -2,37 +2,34 @@ import { Grid, Typography } from "@mui/material";
 import bldg from "./bldg.png";
 import eagle from "./eagle.png";
 
-
 export function HomeImagesCard() {
-
-  
-
   return (
     <>
-      <Grid container sx={{ bgcolor: "#e7ebee" }}>
-        <Grid item sm={7} sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <img
-            alt="bldg"
-            src={bldg}
-            width="auto"
-            // height={460}
-            style={{
-              // alignSelf: "flex-end",
-              minWidth: 350,
-              flexGrow: 0,
-              // marginInline: 10,
-            }}
-          />
-        </Grid>
-
+      <Grid
+        container
+        sx={{
+          bgcolor: "#e7ebee",
+          flexDirection: "row-reverse",
+          justifyContent: "center",
+        }}
+      >
         <Grid item sm={5} alignSelf="center">
           <Typography textAlign="center" variant="h4" color="text.primary">
             TRENTON METRO AREA LOCAL
           </Typography>
         </Grid>
+        <Grid item sm={7} sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <img
+            alt="bldg"
+            src={bldg}
+            width="100%"
+            max-width="400"
+            height="auto"
+          />
+        </Grid>
       </Grid>
 
-      <Grid container sx={{ bgcolor:  "#a7bac8" }}>
+      <Grid container sx={{ bgcolor: "#a7bac8" }}>
         <Grid item sm={5} alignSelf="center">
           <Typography
             variant="h5"
@@ -56,14 +53,9 @@ export function HomeImagesCard() {
           <img
             alt="bldg"
             src={eagle}
-            // width="auto"
-            // height={460}
-            style={{
-              // alignSelf: "flex-start",
-              minWidth: 350,
-              flexGrow: 0,
-              // marginInline: 10,
-            }}
+            width="100%"
+            max-width="400"
+            height="auto"
           />
         </Grid>
       </Grid>
